ce8f835ce032c34af7fac4a772f2955ca803f0cb
Clodo <claudiobidau@gmail.com>
2016-05-24 16:39:37 -0300

    part 6: convert our service and components to use Http

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -9,10 +9,13 @@ import { HeroService } from './hero.service';
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
   styleUrls: ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
+  addingHero = false;
+  error: any;
 
   constructor(
     private heroService: HeroService,
@@ -22,6 +25,28 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
+
+  addHero() {
+    this.addingHero = true;
+    this.selectedHero = null;
+  }
+
+  close(savedHero: Hero) {
+    this.addingHero = false;
+    if (savedHero) { this.getHeroes(); }
+  }
+
+  delete(hero: Hero, event: any) {
+    event.stopPropagation();
+    this.heroService
+        .delete(hero)
+        .then(res => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        })
+        .catch(error => this.error = error); // TODO: Display error message
+  }
+
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
@@ -34,6 +59,5 @@ export class HeroesComponent implements OnInit {
 
   getHeroes() {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-    //this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 }
